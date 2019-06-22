518036f686fc89e0e62dfb9e17a03b8ef83a1634
ogg130 <ogg130@gmail.com>
2019-06-22 11:33:49 -0700

    Update Extensions.cs

--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -4,9 +4,11 @@ namespace System
 {
     public static class Extensions
     {
-        // Used to convert a list of list of anything into chunks determed by the size parameter
+
         // We cannot take credit for this brilliance but we can credit the author:
-        // https://stackoverflow.com/questions/30247968/splitting-a-list-or-collection-into-chunks
+        // https://stackoverflow.com/a/30248074/7147234
+        
+        // Used to convert a list of list of anything into chunks determed by the size parameter
         public static List<List<T>> SplitList<T>(this List<T> me, int size = 3)
         {
             var list = new List<List<T>>();
